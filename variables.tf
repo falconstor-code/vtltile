e70487c80933cf31cd3dfdfd4598e91b5fa7704f
falconstor <110676899+falconstor-code@users.noreply.github.com>
2023-06-09 15:51:43 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,50 +3,50 @@ variable "crn" {
   default     = ""
   description = "Power Systems Virtual Server CRN"
 }
-variable "license_repository_capacity" {
+variable "repository_capacity" {
   type        = number
   default     = 1
-  description = "The VTL licensed repository capacity in TB"
+  description = "The repository capacity in TB"
 }
 variable "memory" {
   type        = number
   default     = 18
-  description = "The amount of memory to assign to the VTL in GB according to the following formula: memory >= 16 + (2 * license_repository_capacity)"
+  description = "The amount of memory to assign to the virtual machine instance in GB according to the following formula: memory >= 16 + (2 * repository_capacity)"
 }
-variable "processors" {
+variable "vcpus" {
   type        = number
   default     = 1
-  description = "The number of vCPUs, AKA virtual processors, to assign to the VTL virtual machine instance; one vCPU is equal to one physical CPU core."
+  description = "The number of vCPUs, AKA virtual processors, to assign to the virtual machine instance; one vCPU is equal to one physical CPU core."
 }
 variable "instance_name" {
   type        = string
   default     = ""
-  description = "The name to assign to the VTL instance"
+  description = "The name to assign to the StorSafe instance"
 }
 variable "processor_type" {
   type        = string
   default     = "shared"
-  description = "The type of processor mode in which the VTL will run: 'shared', 'capped', or 'dedicated'"
+  description = "The type of processor mode in which the instance will run: 'shared', 'capped', or 'dedicated'"
 }
-variable "sys_type" {
+variable "system_type" {
   type        = string
   default     = "s922"
-  description = "The type of system on which to create the VTL: 's922', 'e980'"
+  description = "The type of system on which to create the instance: 's922', 'e980'"
 }
 variable "storage_type" {
   type        = string
   default     = "tier1"
-  description = "The type of storage tier for used volumes: 'tier1' (high performance) or 'tier3'"
+  description = "The type of storage tier for volumes: 'tier1' (high performance) or 'tier3'"
 }
 variable "ssh_key_name" {
   type        = string
   default     = ""
-  description = "The name of the public SSH RSA key to access the VTL instance, as defined for the selected Power Systems Virtual Server CRN"
+  description = "The name of the public SSH RSA key to access the instance, as defined for the selected Power Systems Virtual Server CRN"
 }
 variable "network_1" {
   type        = string
   default     = ""
-  description = "The first network ID or name to assign to the VTL instance, as defined for the selected Power Systems Virtual Server CRN"
+  description = "The first network ID or name to assign to the instance, as defined for the selected Power Systems Virtual Server CRN"
 }
 variable "network_1_ip" {
   type        = string
@@ -56,7 +56,7 @@ variable "network_1_ip" {
 variable "network_2" {
   type        = string
   default     = ""
-  description = "The second network ID or name to assign to the VTL instance, as defined for the selected Power Systems Virtual Server CRN"
+  description = "The second network ID or name to assign to the instance, as defined for the selected Power Systems Virtual Server CRN"
 }
 variable "network_2_ip" {
   type        = string
@@ -66,7 +66,7 @@ variable "network_2_ip" {
 variable "network_3" {
   type        = string
   default     = ""
-  description = "The third network ID or name to assign to the VTL instance, as defined for the selected Power Systems Virtual Server CRN"
+  description = "The third network ID or name to assign to the instance, as defined for the selected Power Systems Virtual Server CRN"
 }
 variable "network_3_ip" {
   type        = string
@@ -76,30 +76,30 @@ variable "network_3_ip" {
 variable "placement_group" {
   type        = string
   default     = ""
-  description = "The server placement group name where the VTL instance will be placed, as defined for the selected Power Systems Virtual Server CRN"
+  description = "The server placement group name where the instance will be placed, as defined for the selected Power Systems Virtual Server CRN"
 }
-variable "affinity_policy" {
+variable "policy_affinity" {
   type        = string
   default     = "anti-affinity"
-  description = "The storage anti-affinity policy to use for placemant of the VTL volume if PVM instance IDs are sepcified"
+  description = "The storage anti-affinity policy to use for placemant of the instance volume if PVM instance IDs are sepcified"
 }
-variable "pvm_instances" {
+variable "pvm_anti_affinity" {
   type        = string
   default     = ""
-  description = "The comma-separated list of PVM instance IDs for the storage anti-affinity policy, as defined for the selected Power Systems Virtual Server CRN"
+  description = "The comma-separated list of other Power Virtual Server (PVM) instance IDs to apply the storage anti-affinity policy for placemant of the StorSafe instance volume, as defined for the selected Power Systems Virtual Server CRN"
 }
 variable "volume_configuration_size" {
   type        = number
   default     = 20
-  description = "The size of the block storage volume for VTL Configuration Repository in GB"
+  description = "The size of the block storage volume for the StorSafe Configuration Repository in GB"
 }
 variable "volume_index_size" {
   type        = number
   default     = 1024
-  description = "The size of the block storage volume for the index of VTL Deduplication Repository in GB; the maximum size of a volume is 2 TB"
+  description = "The size of the block storage volume for the index of StorSafe Deduplication Repository in GB; the maximum size of a volume is 2 TB; attach extra volumes later, if necessary"
 }
 variable "volume_tape_size" {
   type        = number
   default     = 1024
-  description = "The size of the block storage volume for VTL tape backup cache in GB; the maximum size of a volume is 2 TB"
+  description = "The size of the block storage volume for the StorSafe tape backup cache in GB; the maximum size of a volume is 2 TB; attach extra volumes later, if necessary"
 }
